0d772373dd4ba2dfb958d5830563a8c4bc7f3ed0
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2017-08-26 12:03:33 +0300

    Use ${IMPORT} instead of git repo

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ export PATH=~/go/bin:$PATH
 go get github.com/kardianos/govendor
 mkdir ~/go/src/${DIRNAME}
 pushd ~/go/src/${DIRNAME}
-git clone --depth 1 --branch v${VERSION} https://github.com/influxdata/influxdb.git ${BASENAME}-${VERSION}
+git clone --depth 1 --branch v${VERSION} https://${IMPORT}.git ${BASENAME}-${VERSION}
 pushd ${BASENAME}-${VERSION}
 govendor migrate
 govendor sync
