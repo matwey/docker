3335e9b490bc097981c449203c56372a23c405ae
Matwey V. Kornilov <matwey@sai.msu.ru>
2019-01-07 12:32:28 +0300

    Add TianoCore

--- install.sh
+++ install.sh
@@ -3,5 +3,13 @@
 set -e
 set -x
 
-zypper -n --gpg-auto-import-keys install --no-recommends --auto-agree-with-licenses --force-resolution bc cross-aarch64-gcc7 cross-arm-none-gcc7 cross-arm-none-newlib-devel gcc git gzip libopenssl-devel make patch tar which
+zypper -n --gpg-auto-import-keys install --no-recommends --auto-agree-with-licenses --force-resolution bc cross-aarch64-gcc7 cross-arm-none-gcc7 cross-arm-none-newlib-devel gcc gcc-c++ git gzip libopenssl-devel libuuid-devel make nasm patch python tar which
 zypper -n clean -a
+
+pushd /usr/src
+git clone --depth 1 --branch atf-v1.5-armada-18.12 https://github.com/MarvellEmbeddedProcessors/atf-marvell.git atf
+git clone --depth 1 --branch u-boot-2018.03-armada-18.12 https://github.com/MarvellEmbeddedProcessors/u-boot-marvell.git u-boot
+git clone --depth 1 --branch mv_ddr-armada-18.12 https://github.com/MarvellEmbeddedProcessors/mv-ddr-marvell.git mv-ddr
+git clone --depth 1 --branch A3700_utils-armada-18.12 https://github.com/MarvellEmbeddedProcessors/A3700-utils-marvell.git A3700-utils
+git clone --depth 1 --branch uefi-2.7-armada-18.12 https://github.com/MarvellEmbeddedProcessors/uefi-marvell.git edk2
+popd
--- run.sh
+++ run.sh
@@ -3,18 +3,32 @@
 set -e
 set -x
 
-export BL33=/build/u-boot.bin
+FIRMWARE=$1
+
+if [ "x$FIRMWARE" == "xtianocore" ] ; then
+	export BL33=/Build/Armada37x0Db-AARCH64/RELEASE_GCC5/FV/ARMADA_EFI.fd
+else
+	export BL33=/build/u-boot.bin
+fi
+
 export MV_DDR_PATH=/usr/src/mv-ddr
+export GCC5_AARCH64_PREFIX=aarch64-suse-linux-
+export PACKAGES_PATH=/usr/src/edk2:/usr/src/edk2/edk2-platforms
+export EDK_TOOLS_PATH=/usr/src/edk2/BaseTools
 
-pushd /usr/src
-git clone --depth 1 --branch atf-v1.5-armada-18.12 https://github.com/MarvellEmbeddedProcessors/atf-marvell.git atf
-git clone --depth 1 --branch u-boot-2018.03-armada-18.12 https://github.com/MarvellEmbeddedProcessors/u-boot-marvell.git u-boot
-git clone --depth 1 --branch mv_ddr-armada-18.12 https://github.com/MarvellEmbeddedProcessors/mv-ddr-marvell.git mv-ddr
-git clone --depth 1 --branch A3700_utils-armada-18.12 https://github.com/MarvellEmbeddedProcessors/A3700-utils-marvell.git A3700-utils
+if [ "x$FIRMWARE" == "xtianocore" ] ; then
+	set --
+	source /usr/src/edk2/edksetup.sh
+	make -C /usr/src/edk2/BaseTools
+	build -b RELEASE -a AARCH64 -t GCC5 -p Platform/Marvell/Armada37x0Db/Armada37x0Db.dsc
+else
+	make -C /usr/src/u-boot mvebu_espressobin-88f3720_defconfig
+	make -C /usr/src/u-boot DEVICE_TREE=armada-3720-espressobin PLATFORM_LIBGCC=/usr/local/lib/libgcc.a
+fi
+
+make -C /usr/src/atf DEBUG=0 USE_COHERENT_MEM=0 LOG_LEVEL=20 SECURE=0 CLOCKSPRESET=CPU_1000_DDR_800 DDR_TOPOLOGY=2 BOOTDEV=SATA PARTNUM=0 WTP=/usr/src/A3700-utils PLAT=a3700 all fip
+
+pushd /usr/src/atf/build/a3700/release
+install -m 0644 flash-image.bin /boot/flash-image.bin
+install -m 0644 uart-images.tgz /boot/uart-images.tgz
 popd
-
-make -C /usr/src/u-boot mvebu_espressobin-88f3720_defconfig
-make -C /usr/src/u-boot DEVICE_TREE=armada-3720-espressobin PLATFORM_LIBGCC=/usr/local/lib/libgcc.a
-make -C /usr/src/atf DEBUG=1 USE_COHERENT_MEM=0 LOG_LEVEL=20 SECURE=0 CLOCKSPRESET=CPU_1000_DDR_800 DDR_TOPOLOGY=2 BOOTDEV=SATA PARTNUM=0 WTP=/usr/src/A3700-utils PLAT=a3700 all fip
-
-install -m 0644 /usr/src/atf/build/a3700/debug/flash-image.bin /boot/flash-image.bin
