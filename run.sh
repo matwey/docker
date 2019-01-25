660e2dc5ac7c20d2c3f2a2fbb89a8b3058cda312
Matwey V. Kornilov <matwey@sai.msu.ru>
2019-01-25 21:45:41 +0300

    Use zinstall for armv7l

--- run.sh
+++ run.sh
@@ -4,16 +4,18 @@ set -e
 set -x
 
 make -C /usr/src/linux modules_install
-make -C /usr/src/linux install
 
 case "$(uname -m)" in
 	armv*)
+		make -C /usr/src/linux zinstall
 		install -m 0755 /usr/local/lib/grub.efi /boot/bootarm.efi
 	;;
 	aarch64)
+		make -C /usr/src/linux install
 		install -m 0755 /usr/local/lib64/grub.efi /boot/bootaa64.efi
 	;;
 	*)
+		make -C /usr/src/linux install
 		install -m 0755 /usr/lib64/efi/shim.efi /boot/bootx64.efi
 		install -m 0755 /usr/local/lib64/grub.efi /boot/grub.efi
 		install -m 0755 /usr/local/lib/grub.pxe /boot/pxelinux.0
