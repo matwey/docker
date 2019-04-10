5750b7f112575ebf5b12615d2632cb40acf4f1f4
Matwey V. Kornilov <matwey@sai.msu.ru>
2019-04-10 21:23:49 +0300

    Initial commit for u-boot-rk3328

--- run.sh
+++ run.sh
@@ -3,32 +3,19 @@
 set -e
 set -x
 
-FIRMWARE=$1
+make -C /usr/src/atf DEBUG=0 V=1 DISABLE_PEDANTIC=1 PLAT=rk3328 all
 
-if [ "x$FIRMWARE" == "xtianocore" ] ; then
-	export BL33=/Build/Armada37x0Db-AARCH64/DEBUG_GCC5/FV/ARMADA_EFI.fd
-else
-	export BL33=/build/u-boot.bin
-fi
+make -C /usr/src/u-boot rk3328_defconfig
+echo CONFIG_EFI_LOADER=y >> /build/.config
+echo CONFIG_CMD_BOOTEFI=y >> /build/.config
+make -C /usr/src/u-boot oldconfig
 
-export MV_DDR_PATH=/usr/src/mv-ddr
-export GCC5_AARCH64_PREFIX=aarch64-suse-linux-
-export PACKAGES_PATH=/usr/src/edk2:/usr/src/edk2/edk2-platforms
-export EDK_TOOLS_PATH=/usr/src/edk2/BaseTools
+install -D -m 0644 /usr/src/atf/build/rk3328/release/bl31/bl31.elf ${KBUILD_OUTPUT}/bl31.elf
 
-if [ "x$FIRMWARE" == "xtianocore" ] ; then
-	set --
-	source /usr/src/edk2/edksetup.sh
-	make -C /usr/src/edk2/BaseTools
-	build -b DEBUG -a AARCH64 -t GCC5 -p Platform/Marvell/Armada37x0Db/Armada37x0Db.dsc
-else
-	make -C /usr/src/u-boot mvebu_espressobin-88f3720_defconfig
-	make -C /usr/src/u-boot DEVICE_TREE=armada-3720-espressobin PLATFORM_LIBGCC=/usr/local/lib/libgcc.a
-fi
+make -C /usr/src/u-boot PLATFORM_LIBGCC=/usr/local/lib/libgcc.a all
+make -C /usr/src/u-boot PLATFORM_LIBGCC=/usr/local/lib/libgcc.a u-boot.itb
 
-make -C /usr/src/atf DEBUG=0 USE_COHERENT_MEM=0 LOG_LEVEL=20 SECURE=0 CLOCKSPRESET=CPU_1000_DDR_800 DDR_TOPOLOGY=2 BOOTDEV=SATA PARTNUM=0 WTP=/usr/src/A3700-utils PLAT=a3700 all fip
-
-pushd /usr/src/atf/build/a3700/release
-install -m 0644 flash-image.bin /boot/flash-image.bin
-install -m 0644 uart-images.tgz /boot/uart-images.tgz
-popd
+#${KBUILD_OUTPUT}/tools/mkimage -n rk3328 -T rksd -d ${KBUILD_OUTPUT}/tpl/u-boot-tpl.bin /boot/idbloader.img
+${KBUILD_OUTPUT}/tools/mkimage -n rk3328 -T rksd -d /usr/src/rkbin/bin/rk33/rk3328_ddr_333MHz_v1.14.bin /boot/idbloader.img
+cat ${KBUILD_OUTPUT}/spl/u-boot-spl.bin >> /boot/idbloader.img
+install -D -m 0644 ${KBUILD_OUTPUT}/u-boot.itb /boot/u-boot.itb
