313bbc47a26a82de1dc81d51589532e28fac7e83
Matwey V. Kornilov <matwey@sai.msu.ru>
2019-05-30 20:49:07 +0300

    Switch to rock64 branch from http://github.com/matwey/u-boot

--- install.sh
+++ install.sh
@@ -3,26 +3,15 @@
 set -e
 set -x
 
-zypper -n --gpg-auto-import-keys install --no-recommends --auto-agree-with-licenses --force-resolution bc cross-aarch64-gcc7 cross-arm-none-gcc7 cross-arm-none-newlib-devel gcc gcc-c++ git gzip dtc libopenssl-devel libuuid-devel make nasm patch python python-devel python-pip swig tar which
+zypper -n --gpg-auto-import-keys install --no-recommends --auto-agree-with-licenses --force-resolution bc bison cross-aarch64-gcc7 cross-arm-none-gcc7 cross-arm-none-newlib-devel flex gcc gcc-c++ git gzip dtc libopenssl-devel libuuid-devel make nasm patch python python-devel python-pip swig tar which
 zypper -n clean -a
 
 pip install pyelftools
 
 pushd /usr/src
 git config --global user.email "matwey.kornilov@gmail.com"
-git clone --branch next-dev https://github.com/rockchip-linux/u-boot.git u-boot
-git --git-dir=$PWD/u-boot/.git remote add opensuse https://github.com/openSUSE/u-boot.git
-git --git-dir=$PWD/u-boot/.git fetch opensuse master
-git --git-dir=$PWD/u-boot/.git fetch opensuse tumbleweed
-git --git-dir=$PWD/u-boot/.git cherry-pick -x a8606ef0757b722e33c98858c0e4cc6a07b41867 \
-	c80214ce1f39a9b9da32dbe941ff83051b03c080 \
-	a80a232e965acc08d5323b2acc55da855adcf4f3 \
-	9309a1b76ce4f18fe1d9fe48f2b1356ebc58b62f \
-	adae4313cdd891e5ab76c407134b69bd825220e4 \
-	884bcf6f65c414dce3b3d2a91e2c9eba0e5e08f8 \
-	77511b3b4be449f1547cd97ec6dde780ef589d84 \
-	b66c60dde9d48889b93694326d40f7e5208cff25 \
-	257d552be83fd5507780e1f4c09545aab9ea7689
+#git clone --branch next-dev https://github.com/rockchip-linux/u-boot.git u-boot
+git clone --branch rock64_v3 https://github.com/matwey/u-boot.git u-boot
 git clone --depth 1 --branch v1.6 https://github.com/ARM-software/arm-trusted-firmware.git atf
 git clone --depth 1 https://github.com/rockchip-linux/rkbin.git rkbin
 popd
--- run.sh
+++ run.sh
@@ -5,10 +5,7 @@ set -x
 
 make -C /usr/src/atf DEBUG=0 V=1 DISABLE_PEDANTIC=1 PLAT=rk3328 all
 
-make -C /usr/src/u-boot rk3328_defconfig
-echo CONFIG_EFI_LOADER=y >> /build/.config
-echo CONFIG_CMD_BOOTEFI=y >> /build/.config
-make -C /usr/src/u-boot oldconfig
+make -C /usr/src/u-boot rock64-rk3328_defconfig
 
 install -D -m 0644 /usr/src/atf/build/rk3328/release/bl31/bl31.elf ${KBUILD_OUTPUT}/bl31.elf
 
@@ -16,6 +13,6 @@ make -C /usr/src/u-boot PLATFORM_LIBGCC=/usr/local/lib/libgcc.a all
 make -C /usr/src/u-boot PLATFORM_LIBGCC=/usr/local/lib/libgcc.a u-boot.itb
 
 #${KBUILD_OUTPUT}/tools/mkimage -n rk3328 -T rksd -d ${KBUILD_OUTPUT}/tpl/u-boot-tpl.bin /boot/idbloader.img
-${KBUILD_OUTPUT}/tools/mkimage -n rk3328 -T rksd -d /usr/src/rkbin/bin/rk33/rk3328_ddr_333MHz_v1.14.bin /boot/idbloader.img
+${KBUILD_OUTPUT}/tools/mkimage -n rk3328 -T rksd -d /usr/src/rkbin/bin/rk33/rk3328_ddr_333MHz_v1.16.bin /boot/idbloader.img
 cat ${KBUILD_OUTPUT}/spl/u-boot-spl.bin >> /boot/idbloader.img
 install -D -m 0644 ${KBUILD_OUTPUT}/u-boot.itb /boot/u-boot.itb
