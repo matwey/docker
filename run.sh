ba0d682edd567ce329a6ac174ac1192db1c305a9
Matwey V. Kornilov <matwey@sai.msu.ru>
2019-07-25 21:35:09 +0300

    Add u-boot TPL

--- run.sh
+++ run.sh
@@ -3,7 +3,13 @@
 set -e
 set -x
 
-${KBUILD_OUTPUT}/tools/mkimage -n rk3328 -T rksd -d /usr/src/rkbin/bin/rk33/rk3328_ddr_333MHz_v1.16.bin /boot/idbloader.img
+if [[ -f "/build/tpl/u-boot-tpl.bin" ]]
+then
+	TPL_BINARY="/build/tpl/u-boot-tpl.bin"
+else
+	TPL_BINARY="/usr/src/rkbin/bin/rk33/rk3328_ddr_333MHz_v1.16.bin"
+fi
+${KBUILD_OUTPUT}/tools/mkimage -n rk3328 -T rksd -d ${TPL_BINARY} /boot/idbloader.img
 cat ${KBUILD_OUTPUT}/spl/u-boot-spl.bin >> /boot/idbloader.img
 install -D -m 0644 ${KBUILD_OUTPUT}/u-boot.itb /boot/u-boot.itb
 
